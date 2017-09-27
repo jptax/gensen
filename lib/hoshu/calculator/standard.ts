889083885373dcbf3afef3c4586ea05ec27410f7
mugi-uno <mugi.uno@gmail.com>
2017-09-28 00:12:01 +0900

    手取契約の場合の源泉徴収税額の計算ロジックを追加

--- lib/hoshu/calculator/standard.ts
+++
@@ -1,6 +0,0 @@
-export default function (kingaku: number) : GensenReponse {
-  return {
-    gensen: 100,
-    shiharai: 2000
-  };
-}
--- test/hoshu/calculator/standard.ts
+++
@@ -1,6 +0,0 @@
-import test from 'ava';
-import standard from '../../../lib/hoshu/calculator/standard';
-
-test((t) => {
-	t.deepEqual(standard(100), { gensen: 100, shiharai: 2000 });
-});
