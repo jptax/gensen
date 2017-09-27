c1fecc9b2ddaa1e44b4930cea3f9a7030f0739d8
corocn <crsk.plmzh@gmail.com>
2017-09-28 00:35:45 +0900

    コンフリ解消

--- lib/hoshu/calculator/calculator.ts
+++ lib/hoshu/calculator/calculator.ts
@@ -2,8 +2,9 @@ import tedori from './tedori';
 import shihoushoshi from './shihoushoshi';
 import genkouKouen from './genkou-kouen';
 
+
 export default {
-  genkouKouen,
   tedori,
-  shihoushoshi
+  shihoushoshi,
+  genkouKouen
 }
