4bcaa2f994e9b479d2757a6b998ae3e8dd711f22
corocn <crsk.plmzh@gmail.com>
2017-09-28 09:39:41 +0900

    Kingakuレスポンスに合わせて司法書士の計算を修正

--- test/hoshu/calculator/shihoushoshi.test.ts
+++ test/hoshu/calculator/shihoushoshi.test.ts
@@ -5,21 +5,50 @@ import gensen from '../../../lib/index';
 // https://www.nta.go.jp/taxanswer/gensen/2801.htm
 
 test('国税局のサンプル例', (t) => {
-  t.deepEqual(gensen.hoshu.shihoushoshi(50000), { gensen: 4084, shiharai: 50000 });
+  t.deepEqual(
+    gensen.hoshu.shihoushoshi(50000),
+    {
+      zei: 4084,
+      zeikomi: 50000,
+      zeinuki: 45916
+    });
 });
 
 test('1万円を引いた時にマイナスであれば源泉徴収は0とする', (t) => {
-  t.deepEqual(gensen.hoshu.shihoushoshi(9999), { gensen: 0, shiharai: 9999 });
+  t.deepEqual(
+    gensen.hoshu.shihoushoshi(9999),
+    {
+      zei: 0,
+      zeikomi: 9999,
+      zeinuki: 9999
+    });
 });
 
 test('1万円ちょうどの場合', (t) => {
-  t.deepEqual(gensen.hoshu.shihoushoshi(10000), { gensen: 0, shiharai: 10000 });
+  t.deepEqual(
+    gensen.hoshu.shihoushoshi(10000),
+    {
+      zei: 0,
+      zeikomi: 10000,
+      zeinuki: 10000
+    });
 });
 
 test('小数点以下切り捨て', (t) => {
-  t.deepEqual(gensen.hoshu.shihoushoshi(50001), { gensen: 4084, shiharai: 50001 });
+  t.deepEqual(
+    gensen.hoshu.shihoushoshi(50001),
+    {
+      zei: 4084,
+      zeikomi: 50001,
+      zeinuki: 45917
+    });
 });
 
 test('100万円を超える場合も、10.21%で計算される', (t) => {
-  t.deepEqual(gensen.hoshu.shihoushoshi(1000001), { gensen: 101079, shiharai: 1000001 });
+  t.deepEqual(gensen.hoshu.shihoushoshi(1000001),
+    {
+      zei: 101079,
+      zeikomi: 1000001,
+      zeinuki: 898922
+    });
 });
