0e12644965f65a368fc32555439b1b46150f4181
RenyaMizuno <renyamizuno@gmail.com>
2017-09-28 14:18:26 +0900

    プロボクサーの業務に関する報酬・料金についての源泉徴収のロジックを追加
    http://www.nta.go.jp/shiraberu/ippanjoho/pamph/gensen/shikata2017/pdf/09.pdf

--- lib/hoshu/calculator/calculator.ts
+++ lib/hoshu/calculator/calculator.ts
@@ -3,6 +3,7 @@ import gaikouin from "./gaikouin";
 import genkouKouen from "./genkou-kouen";
 import hostess from "./hostess";
 import koukokuSyoukin from "./koukoku-syoukin";
+import proBoxer from "./pro-boxer";
 import senzokuKeiyakukin from "./senzoku-keiyakukin";
 import shihoushoshi from "./shihoushoshi";
 import tedori from "./tedori";
@@ -13,6 +14,7 @@ export default {
   genkouKouen,
   hostess,
   koukokuSyoukin,
+  proBoxer,
   senzokuKeiyakukin,
   shihoushoshi,
   tedori,
---
+++ test/hoshu/calculator/pro-boxer.test.ts
@@ -0,0 +1,16 @@
+import test from "ava";
+import proBoxer from "../../../lib/hoshu/calculator/pro-boxer";
+
+// プロボクサーの業務に関する報酬・料金
+// http://www.nta.go.jp/shiraberu/ippanjoho/pamph/gensen/shikata2017/pdf/09.pdf
+
+test("50,000以下の場合", (t) => {
+  t.deepEqual(proBoxer(30000), { zei: 0, zeinuki: 30000, zeikomi: 30000 });
+  t.deepEqual(proBoxer(50000), { zei: 0, zeinuki: 50000, zeikomi: 50000 });
+});
+
+test("50,000を超える場合", (t) => {
+  t.deepEqual(proBoxer(50001), { zei: 0, zeinuki: 50001, zeikomi: 50001 });
+  t.deepEqual(proBoxer(100000), { zei: 5105, zeinuki: 94895, zeikomi: 100000 });
+  t.deepEqual(proBoxer(500000), { zei: 45945, zeinuki: 454055, zeikomi: 500000 });
+});
