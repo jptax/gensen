7f96d04281fc7de6708e5f4932e5d4cebec44e22
corocn <crsk.plmzh@gmail.com>
2017-09-28 11:01:36 +0900

    外交員のテストに境界値テストを追加

--- test/hoshu/calculator/gaikouin.test.ts
+++ test/hoshu/calculator/gaikouin.test.ts
@@ -4,7 +4,7 @@ import gensen from "../../../lib/index";
 // No.2804　外交員等に支払う報酬・料金
 // https://www.nta.go.jp/taxanswer/gensen/2804.htm
 
-test("報酬・料金を20万円支払う場合", (t) => {
+test("報酬・料金を20万円支払う場合（控除フル適用）", (t) => {
   t.deepEqual(
     gensen.hoshu.gaikouin(200000),
     {
@@ -14,7 +14,7 @@ test("報酬・料金を20万円支払う場合", (t) => {
     });
 });
 
-test("報酬・料金20万円と給与5万円を支払う場合", (t) => {
+test("報酬・料金20万円と給与5万円を支払う場合（控除から給与分引かれる）", (t) => {
   t.deepEqual(
     gensen.hoshu.gaikouin(200000, 50000),
     {
@@ -24,9 +24,39 @@ test("報酬・料金20万円と給与5万円を支払う場合", (t) => {
     });
 });
 
-test("報酬・料金20万円と給与15万円を支払う場合", (t) => {
+test("報酬・料金20万円と給与15万円を支払う場合（控除0円）", (t) => {
   t.deepEqual(
     gensen.hoshu.gaikouin(200000, 150000),
+    {
+      zei: 20420,
+      zeikomi: 200000,
+      zeinuki: 179580,
+    });
+});
+
+test("報酬・料金20万円と給与119999円を支払う場合", (t) => {
+  t.deepEqual(
+    gensen.hoshu.gaikouin(200000, 119999),
+    {
+      zei: 20419,
+      zeikomi: 200000,
+      zeinuki: 179581,
+    });
+});
+
+test("報酬・料金20万円と給与12万円を支払う場合", (t) => {
+  t.deepEqual(
+    gensen.hoshu.gaikouin(200000, 120000),
+    {
+      zei: 20420,
+      zeikomi: 200000,
+      zeinuki: 179580,
+    });
+});
+
+test("報酬・料金20万円と給与120001円を支払う場合", (t) => {
+  t.deepEqual(
+    gensen.hoshu.gaikouin(200000, 120001),
     {
       zei: 20420,
       zeikomi: 200000,
