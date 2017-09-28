5e45a87e2ad193aa85a1dfc180ca975738460c84
corocn <crsk.plmzh@gmail.com>
2017-09-28 10:23:40 +0900

    外交員の計算ロジックを追加

--- lib/hoshu/calculator/calculator.ts
+++ lib/hoshu/calculator/calculator.ts
@@ -1,9 +1,11 @@
 import genkouKouen from "./genkou-kouen";
 import shihoushoshi from "./shihoushoshi";
 import tedori from "./tedori";
+import gaikouin from "./gaikouin";
 
 export default {
   genkouKouen,
   shihoushoshi,
   tedori,
+  gaikouin,
 };
---
+++ test/hoshu/calculator/gaikouin.test.ts
@@ -0,0 +1,35 @@
+import test from 'ava';
+import gensen from '../../../lib/index';
+
+// No.2804　外交員等に支払う報酬・料金
+// https://www.nta.go.jp/taxanswer/gensen/2804.htm
+
+test('報酬・料金を20万円支払う場合', (t) => {
+  t.deepEqual(
+    gensen.hoshu.gaikouin(200000),
+    {
+      zei: 8168,
+      zeikomi: 200000,
+      zeinuki: 191832
+    });
+});
+
+test('報酬・料金20万円と給与5万円を支払う場合', (t) => {
+  t.deepEqual(
+    gensen.hoshu.gaikouin(200000, 50000),
+    {
+      zei: 13273,
+      zeikomi: 200000,
+      zeinuki: 186727
+    });
+});
+
+test('報酬・料金20万円と給与15万円を支払う場合', (t) => {
+  t.deepEqual(
+    gensen.hoshu.gaikouin(200000, 150000),
+    {
+      zei: 20420,
+      zeikomi: 200000,
+      zeinuki: 179580
+    });
+});
