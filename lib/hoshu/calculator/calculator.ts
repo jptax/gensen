e2691b1d43c0b210919c498eb755af383e23455b
corocn <crsk.plmzh@gmail.com>
2017-09-28 13:24:05 +0900

    広告のための賞金の計算ロジックを追加

--- lib/hoshu/calculator/calculator.ts
+++ lib/hoshu/calculator/calculator.ts
@@ -3,6 +3,7 @@ import gaikouin from "./gaikouin";
 import genkouKouen from "./genkou-kouen";
 import hostess from "./hostess";
 import senzokuKeiyakukin from "./senzoku-keiyakukin";
+import koukokuSyoukin from "./koukoku-syoukin";
 import shihoushoshi from "./shihoushoshi";
 import tedori from "./tedori";
 
@@ -12,6 +13,7 @@ export default {
   genkouKouen,
   hostess,
   senzokuKeiyakukin,
+  koukokuSyoukin,
   shihoushoshi,
   tedori,
 };
---
+++ test/hoshu/calculator/koukoku-syoukin.test.ts
@@ -0,0 +1,45 @@
+import test from "ava";
+import gensen from "../../../lib/index";
+
+// No.2813　広告宣伝のために支払う賞金等
+// https://www.nta.go.jp/taxanswer/gensen/2813.htm
+
+test("賞金が50万以下の場合は源泉徴収しない", (t) => {
+  t.deepEqual(
+    gensen.hoshu.koukokuSyoukin(10000),
+    {
+      zei: 0,
+      zeikomi: 10000,
+      zeinuki: 10000,
+    });
+});
+
+test("賞金が50万以下の場合は源泉徴収しない（50万ちょうど）", (t) => {
+  t.deepEqual(
+    gensen.hoshu.koukokuSyoukin(500000),
+    {
+      zei: 0,
+      zeikomi: 500000,
+      zeinuki: 500000,
+    });
+});
+
+test("賞金が500001円の場合は50万を差引いて源泉徴収する（ただし税金が0円になる）", (t) => {
+  t.deepEqual(
+    gensen.hoshu.koukokuSyoukin(500001),
+    {
+      zei: 0,
+      zeikomi: 500001,
+      zeinuki: 500001,
+    });
+});
+
+test("賞金が500001円の場合は50万を差引いて源泉徴収する（税金が1円以上）", (t) => {
+  t.deepEqual(
+    gensen.hoshu.koukokuSyoukin(500010),
+    {
+      zei: 1,
+      zeikomi: 500010,
+      zeinuki: 500009,
+    });
+});
