6eb8e0823d62e304ffe6605bd1cc6b3e94a3f819
mugi-uno <mugi.uno@gmail.com>
2017-09-28 09:23:05 +0900

    手取り金額のレスポンスタイプを変更

--- lib/hoshu/calculator/tedori.ts
+++ lib/hoshu/calculator/tedori.ts
@@ -3,24 +3,26 @@ import BN from 'bignumber.js';
 const TEDORI_NIDANKAI_ZEIRITSU_KINGAKU = 897900;
 
 // 手取契約の場合の源泉徴収税額の計算
-export default function (tedori:number) : Kingaku {
-  const t : BN = new BN(tedori);
+export default function (originZeinuki:number) : Kingaku {
+  const zeinuki : BN = new BN(originZeinuki);
 
-  if (t.lte(TEDORI_NIDANKAI_ZEIRITSU_KINGAKU)) {
+  if (zeinuki.lte(TEDORI_NIDANKAI_ZEIRITSU_KINGAKU)) {
     // 二段階税率の適用がある場合
-    const shiharai:BN = t.dividedBy(0.8979).floor();
+    const zeikomi:BN = zeinuki.dividedBy(0.8979).floor();
 
     return {
-      shiharai: shiharai.toNumber(),
-      gensen: shiharai.times(0.1021).floor().toNumber()
+      zei: zeinuki.times(0.1021).floor().toNumber(),
+      zeinuki: zeinuki.toNumber(),
+      zeikomi: zeikomi.toNumber()
     };
   } else {
     // 税率が10.21％の場合
-    const shiharai:BN = t.minus(102100).dividedBy(0.7958).floor();
+    const zeikomi:BN = zeinuki.minus(102100).dividedBy(0.7958).floor();
     
     return {
-      shiharai: shiharai.toNumber(),
-      gensen: shiharai.times(0.1021).floor().toNumber()
+      zei: zeikomi.times(0.1021).floor().toNumber(),
+      zeinuki: zeinuki.toNumber(),
+      zeikomi: zeikomi.toNumber()
     };
   }
 }
--- test/hoshu/calculator/tedori.test.ts
+++ test/hoshu/calculator/tedori.test.ts
@@ -3,10 +3,10 @@ import tedori from '../../../lib/hoshu/calculator/tedori';
 
 // https://www.nta.go.jp/taxanswer/gensen/2792_qa.htm
 test('税率が10.21％の場合（手取額が897,900円以下の場合)', t => {
-  t.deepEqual(tedori(100000), { shiharai: 111370, gensen: 11370 });
-  t.deepEqual(tedori(897900), { shiharai: 1000000, gensen: 102100 });
+  t.deepEqual(tedori(100000), { zei: 11370, zeikomi: 111370, zeinuki: 100000 });
+  t.deepEqual(tedori(897900), { zei: 102100, zeikomi: 1000000, zeinuki: 897900 });
 });
 
 test('二段階税率の適用がある場合（手取額が897,900円超の場合）', t => {
-  t.deepEqual(tedori(897901), { shiharai: 1000001, gensen: 102100 });
+  t.deepEqual(tedori(897901), { zei: 102100, zeikomi: 1000001, zeinuki: 897901 });
 });
